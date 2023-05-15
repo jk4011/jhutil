87fbb9d13e3874c388bc122399f6b0653458b86b
JH <jinhyuk@unist.ac.kr>
2023-05-15 14:14:28 +0900

    .

--- scripts/docker_distribute.sh
+++ scripts/docker_distribute.sh
@@ -4,21 +4,38 @@ set -o xtrace # print command
 
 # command example: 
 # ------------------------------------------------------------------------------------------
-# docker_distribute.sh node4 wlsgur4011/part_assembly node6 node7
+# docker_distribute.sh [host] [container] [tag] [client1 client2 client3]
+# docker_distribute.sh node2 part_assembly latest node4 node6
+
 # ------------------------------------------------------------------------------------------
 
 host=$1
-image=$2
-clients=("${@:3}")
-file_path=/data/wlsgur4011/docker/images/${2//\//:}.tar
-echo "saving $file_path..."
-ssh $host docker save $image -o $file_path
-ssh $host docker save $image -o $file_path
-ssh $host chmod 777 $file_path
-echo "saving done!"
+container=$2
+tag=$3
+image=wlsgur4011/$container:$tag
+clients=("${@:4}")
+
+ssh $host docker commit $container $image
+echo "host commit done!"
+ssh $host docker push $image
+echo "host push done!"
 
 for client in "${clients[@]}"
 do
-    echo "loading $file_path to $client..."
-    ssh $client docker load -i $file_path &
+    ssh $client docker pull $image &
 done
+wait
+
+echo "distribute done!"
+echo ""
+echo "docker run example:"
+echo ""
+
+echo "nvidia-docker run --name $container \
+  -v /data/wlsgur4011:/data/wlsgur4011 \
+  -p 23444:23444 \
+  -p 88:34555 \
+  -e TZ=Asia/Seoul \
+  --ipc host \
+  --restart unless-stopped \
+  -it $image bash" 
