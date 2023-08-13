a6360289e0015166e13f374d5a165594be37e78e
Jinhyeok <jinhyuk@unist.ac.kr>
2023-08-13 22:14:10 +0900

    memory efficient knn

--- jhutil/algorithm.py
+++ jhutil/algorithm.py
@@ -38,10 +38,27 @@ def knn(src, dst, k=1, is_naive=False, is_sklearn=False):
 
         from knn_cuda import KNN
         knn = KNN(k=1, transpose_mode=True)
-        distance, indices = knn(dst[None, :], src[None, :])
+        chunk_size = 1e9
+        src_chunks = torch.chunk(src, int(src.shape[0] * dst.shape[0] // chunk_size) + 1, 0)
+        distance = []
+        indices = []
+        for src_chunk in src_chunks:
+            dist_chunk, indices_chunk = knn(dst[None, :], src_chunk[None, :])
+            distance.append(dist_chunk)
+            indices.append(indices_chunk)
+        distance = torch.cat(distance, dim=1)
+        indices = torch.cat(indices, dim=1)
 
     tmp = distance.ravel()
     distance = tmp.cpu()
     indices = indices.ravel().cpu()
 
     return distance, indices
+
+
+if __name__ == "__main__":
+    src = torch.rand(100000, 3)
+    dst = torch.rand(100000, 3)
+    distance, indices = knn(src, dst)
+    
+    import jhutil; jhutil.jhprint(1111, distance, indices, endline='\n')
