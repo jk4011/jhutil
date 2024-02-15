6e72e12f8bcf49c2c3a8d5c829136230d130b639
JinHyeok <jinhyuk@unist.ac.kr>
2024-02-15 12:59:20 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jhutil",
-    version="0.1.4",
+    version="0.1.5",
     description="jhutil",
     long_description_content_type="text/markdown",
     url="https://github.com/jk4011/jhutil",
