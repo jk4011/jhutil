bc5ddfd4bc2810291075dffc37e49ec79a55e33b
Jinhyeok <jinhyeok@unist.ac.kr>
2024-05-21 14:27:21 +0900

    ✨ disable init import

--- jhutil/__init__.py
+++ jhutil/__init__.py
@@ -1,8 +1,8 @@
 from .logging_utils import *
-from .notion_utils import *
-from .pcd_utils import *
-from .slack_utils import *
-from .depth_utils import *
-from .freq_utils import *
-from .mesh_utils import *
+# from .notion_utils import *
+# from .pcd_utils import *
+# from .slack_utils import *
+# from .depth_utils import *
+# from .freq_utils import *
+# from .mesh_utils import *
 # from .open3d_utils import *
