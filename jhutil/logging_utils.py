b33d45f90b95b5512ed979870a34839114073559
Jinhyeok <jinhyeok@unist.ac.kr>
2024-12-18 13:36:16 +0900

    color_log color changed

--- jhutil/logging_utils.py
+++ jhutil/logging_utils.py
@@ -19,30 +19,29 @@ def enable_color_log():
 def disable_color_log():
     global _disabled
     _disabled = True
+    
 
-
-def color_log(key, *datas, is_yaml=False, endline=' ', force=False, return_str=False):
+def color_log(key, *datas, is_yaml=False, endline=' ', force=False, return_str=False, enable_duplicates=False):
     if force:
         pass
     elif _disabled:
         return
-
     colors = {
         1111: Back.RED + Fore.BLACK,
         2222: Back.YELLOW + Fore.BLACK,
         3333: Back.GREEN + Fore.BLACK,
         4444: Back.CYAN + Fore.BLACK,
         5555: Back.BLUE + Fore.BLACK,
-        6666: Fore.RED,
-        7777: Fore.YELLOW,
-        8888: Fore.GREEN,
-        9999: Fore.CYAN,
+        6666: Back.RED + Fore.BLACK,
+        7777: Back.YELLOW + Fore.BLACK,
+        8888: Back.GREEN + Fore.BLACK,
+        9999: Back.CYAN + Fore.BLACK,
         0: Back.WHITE + Fore.BLACK,
-        "aaaa": Back.RED + Fore.BLACK,
-        "bbbb": Back.YELLOW + Fore.BLACK,
-        "cccc": Back.GREEN + Fore.BLACK,
-        "dddd": Back.CYAN + Fore.BLACK,
-        "eeee": Back.BLUE + Fore.BLACK,
+        "aaaa": Fore.RED,
+        "bbbb": Fore.YELLOW,
+        "cccc": Fore.GREEN,
+        "dddd": Fore.CYAN,
+        "eeee": Fore.BLUE,
         "ffff": Fore.RED,
         "gggg": Fore.YELLOW,
         "hhhh": Fore.GREEN,
@@ -52,7 +51,7 @@ def color_log(key, *datas, is_yaml=False, endline=' ', force=False, return_str=F
         color = colors[key // 1000 * 1111]
     elif isinstance(key, str):
         color = colors[key]
-
+    
     ret_str = ""
     for data in datas:
         try:
@@ -67,18 +66,19 @@ def color_log(key, *datas, is_yaml=False, endline=' ', force=False, return_str=F
 
         ret_str = ret_str + endline + str(data)
 
-    if isinstance(key, int):
+    if key in [6666, 7777, 8888, 9999, "ffff", "gggg", "hhhh", "iiii"]:
+        key = str(key)
+        output = Back.WHITE + Fore.BLACK + " " + key[0:1] + Style.RESET_ALL + \
+            color + f"{key[1:]} {ret_str}" + Style.RESET_ALL
+    else:
         if key == 0:
             key = "0000"
         output = color + f" {key} {ret_str}" + Style.RESET_ALL
-    else:
-        output = Back.WHITE + Fore.BLACK + " " + key[0:2] + Style.RESET_ALL + \
-            color + f"{key[2:]} {ret_str}" + Style.RESET_ALL
 
     print(output)
     if return_str:
         return output
-
+    
 
 def convert_into_json(value):
     value = deepcopy(value)
@@ -95,4 +95,3 @@ def convert_into_json(value):
         return lo(value)
     else:
         return str(value)
-
