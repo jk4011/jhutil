02710653799f951c206445940c0be93b10ced51f
Jinhyeok <jinhyuk@unist.ac.kr>
2025-04-07 14:32:32 +0900

    show groups

--- jhutil/visualize.py
+++ jhutil/visualize.py
@@ -8,6 +8,7 @@ from base64 import b64encode
 from IPython.display import HTML
 from ipywidgets import interact
 import io
+from .pcd_utils import PALATTE
 
 
 def show_video(video_path):
@@ -113,6 +114,76 @@ def show_matching(
 
         axes[0].scatter(mkpts0[:, 0], mkpts0[:, 1], c=color, s=4)
         axes[1].scatter(mkpts1[:, 0], mkpts1[:, 1], c=color, s=4)
+
+    # Convert figure to PIL Image
+    buf = io.BytesIO()
+    fig.savefig(buf, format="PNG", bbox_inches="tight", pad_inches=0)
+    plt.close(fig)  # Close the figure to free memory
+    buf.seek(0)
+    img_pil = Image.open(buf)
+
+    return img_pil
+
+
+def show_groups(
+    img0,
+    img1,
+    mkpts0=torch.empty(0, 2),
+    mkpts1=torch.empty(0, 2),
+    groups=None,
+    kpts0=None,
+    kpts1=None,
+    dpi=75,
+    bbox=None,
+):
+    if img0.shape[0] in [3, 4]:
+        img0 = img0.permute(1, 2, 0)
+        img1 = img1.permute(1, 2, 0)
+    if isinstance(img0, torch.Tensor):
+        img0 = img0.cpu().numpy()
+        img1 = img1.cpu().numpy()
+    if isinstance(mkpts0, torch.Tensor):
+        mkpts0 = mkpts0.cpu().numpy()
+        mkpts1 = mkpts1.cpu().numpy()
+    mkpts0 = mkpts0.copy()
+    mkpts1 = mkpts1.copy()
+
+    if bbox is not None:
+        w_from, h_from, w_to, h_to = bbox
+        img0 = img0[h_from:h_to, w_from:w_to]
+        img1 = img1[h_from:h_to, w_from:w_to]
+        mkpts0[:, 0] = mkpts0[:, 0] - w_from
+        mkpts0[:, 1] = mkpts0[:, 1] - h_from
+        mkpts1[:, 0] = mkpts1[:, 0] - w_from
+        mkpts1[:, 1] = mkpts1[:, 1] - h_from
+
+    assert (
+        mkpts0.shape[0] == mkpts1.shape[0]
+    ), f"mkpts0: {mkpts0.shape[0]} v.s. mkpts1: {mkpts1.shape[0]}"
+    fig, axes = plt.subplots(1, 2, figsize=(10, 6), dpi=dpi)
+    axes[0].imshow(img0, cmap="gray")
+    axes[1].imshow(img1, cmap="gray")
+    for i in range(2):  # clear all frames
+        axes[i].get_yaxis().set_ticks([])
+        axes[i].get_xaxis().set_ticks([])
+        for spine in axes[i].spines.values():
+            spine.set_visible(False)
+    plt.tight_layout(pad=1)
+
+    if kpts0 is not None:
+        assert kpts1 is not None
+        axes[0].scatter(kpts0[:, 0], kpts0[:, 1], c="w", s=2)
+        axes[1].scatter(kpts1[:, 0], kpts1[:, 1], c="w", s=2)
+
+    if mkpts0.shape[0] != 0 and mkpts1.shape[0] != 0:
+        for i in range(len(groups)):
+            group = groups[i]
+            color = (PALATTE[i] / 255).tolist()
+            color = color + [0.1]
+            fig.canvas.draw()
+
+            axes[0].scatter(mkpts0[group][:, 0], mkpts0[group][:, 1], c=color, s=50, marker='s')
+            axes[1].scatter(mkpts1[group][:, 0], mkpts1[group][:, 1], c=color, s=50, marker='s')
 
     # Convert figure to PIL Image
     buf = io.BytesIO()
